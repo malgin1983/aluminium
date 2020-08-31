8ee2c2eda4782c1adb9eb35d517a5a2f28caea73
Malgin Vadim <malghin@yandex.ru>
2020-08-31 23:52:11 +0300

    created page product new section slider

--- public/scripts/slider-product.js
+++ public/scripts/slider-product.js
@@ -1,6 +1,12 @@
 const slides1 = document.getElementsByClassName("item");
 const dots1 = document.getElementsByClassName("slider-dots_item");
+const slides2 = document.getElementsByClassName("fasad");
+const dots2 = document.getElementsByClassName("dots__fasad");
+const slides3 = document.getElementsByClassName("doors");
+const dots3 = document.getElementsByClassName("dots__doors");
 
+
+let slideIndex = 1;
 /* Основная функция слайдера */
 function showSlides(n, slides, dots) {
     let i;
@@ -21,8 +27,8 @@ function showSlides(n, slides, dots) {
     dots[slideIndex - 1].className += " active";
 }
 
-/* Индекс слайда по умолчанию */
-let slideIndex = 1;
+/* Slider Sliders */
+
 showSlides(slideIndex, slides1, dots1);
 
 function plusSlide() {
@@ -33,5 +39,25 @@ function minusSlide() {
     showSlides(slideIndex -= 1, slides1, dots1);
 }
 
+/* Slider Fasad */
+showSlides(slideIndex, slides2, dots2);
 
+function plusSlide() {
+    showSlides(slideIndex += 1, slides2, dots2);
+}
+
+function minusSlide() {
+    showSlides(slideIndex -= 1, slides2, dots2);
+}
+
+/* Slider Doors */
+showSlides(slideIndex, slides3, dots3);
+
+function plusSlide() {
+    showSlides(slideIndex += 1, slides3, dots3);
+}
+
+function minusSlide() {
+    showSlides(slideIndex -= 1, slides3, dots3);
+}
 
